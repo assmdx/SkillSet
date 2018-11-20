1092ec333d5d8aeb04e03f4193b3b86fffd8b32f
assmdx <assmdx183@163.com>
2018-11-20 11:36:54 +0800

    ADD 完成添加技能功能

--- wechat-mini-program/SkillSet/src/mixins/test.js
+++ wechat-mini-program/SkillSet/src/mixins/test.js
@@ -2,10 +2,35 @@ import wepy from 'wepy'
 
 export default class testMixin extends wepy.mixin {
   data = {
-    rootkey:"",
-    rootValue:1,
-    children:[]
-  }
-  methods = {
+    skills:[
+      {
+        name:'skills',
+        children:[1,2],
+        floor:1,
+        status:false, //false 代表折叠，隐藏状态
+        img:"../images/skillCell_down.png"
+      },
+      {
+        name:'react',
+        children:[],
+        floor:2,
+        status:false, //false 代表折叠，隐藏状态
+        img:"../images/skillCell_down.png"
+      },
+      {
+        name:'vue',
+        children:[3],
+        floor:2,
+        status:false, //false 代表折叠，隐藏状态
+        img:"../images/skillCell_down.png"
+      },
+      {
+        name:'wepy',
+        children:[],
+        floor:3,
+        status:false, //false 代表折叠，隐藏状态
+        img:"../images/skillCell_down.png"
+      }
+    ]
   }
 }
