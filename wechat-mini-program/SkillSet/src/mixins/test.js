425cd3c4227c0377daef784e1f3f55668f97097e
assmdx <assmdx183@163.com>
2018-11-22 01:22:17 +0800

    UPDATE 删除package.json中无效的dependency UPDATE 修改生成树节点的位置计算的方法，修复计算父节点错误的bug

--- wechat-mini-program/SkillSet/src/mixins/test.js
+++ wechat-mini-program/SkillSet/src/mixins/test.js
@@ -2,30 +2,32 @@ import wepy from 'wepy'
 
 export default class testMixin extends wepy.mixin {
   data = {
+    showPanel: "",
+    showCanvas: "none",
     skills:[
       {
-        name:'skills',
-        floor:1,
-        showEdit:"none",
-        showAdd:"none"
+        name: 'skills',
+        floor: 1,
+        showEdit: "none",
+        showAdd: "none"
       },
       {
-        name:'react',
-        floor:2,
-        showEdit:"none",
-        showAdd:"none"
+        name: 'react',
+        floor: 2,
+        showEdit: "none",
+        showAdd: "none"
       },
       {
-        name:'vue',
-        floor:2,
-        showEdit:"none",
-        showAdd:"none"
+        name: 'vue',
+        floor: 2,
+        showEdit: "none",
+        showAdd: "none"
       },
       {
-        name:'wepy',
-        floor:3,
-        showEdit:"none",
-        showAdd:"none"
+        name: 'wepy',
+        floor: 3,
+        showEdit: "none",
+        showAdd: "none"
       }
     ]
   }
